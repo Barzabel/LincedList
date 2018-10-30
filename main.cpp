01c350876b2ab9ec911dafafcf4e9f281f612956
Barzabel <kochenko1995@mail.ru>
2018-10-30 20:42:48 +0300

    fixed delall 2

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@ using namespace std;
 template<class T>
 class Node {
 public:
-Node(T value)
-{
-	this->value = value;
-	this->next = nullptr;
-}
+	Node(T value)
+	{
+		this->value = value;
+		this->next = nullptr;
+	}
 	Node<T> *next;
 	T value;
 
 private:
-	
+
 };
 
 template<class T>
@@ -24,21 +24,21 @@ public:
 		this->tail = nullptr;
 	}
 	void add_in_tail(Node<T> *item) {
-	if(this->head==nullptr)
+		if (this->head == nullptr)
 		{
 			this->head = item;
 			this->tail = item;
-			this->tail -> next = nullptr;
+			this->tail->next = nullptr;
 		}
-	else
+		else
 		{
 			this->tail->next = item;
 			this->tail = item;
-			this->tail -> next = nullptr;
+			this->tail->next = nullptr;
 		}
 	}
 
-	T getvalhead() 
+	T getvalhead()
 	{
 		return this->head->value;
 	}
@@ -49,20 +49,20 @@ public:
 	}
 
 	Node<T>* find(T val) {
-		Node<T> (*H) = this->head;
-		while (H!=nullptr){
-			if(H->value==val )
+		Node<T>(*H) = this->head;
+		while (H != nullptr) {
+			if (H->value == val)
 			{
 				return H;
 			}
-			H = H->next;	
-		} 
+			H = H->next;
+		}
 		return nullptr;
 	}
 
-	
 
-	//удаление одного элемента
+
+
 	void del(T val)
 	{
 
@@ -76,9 +76,9 @@ public:
 			this->tail = nullptr;
 			return;
 		}
-		if(this->head->next!=nullptr)
+		if (this->head->next != nullptr)
 		{
-			if (this->head->value == val) 
+			if (this->head->value == val)
 			{
 				this->head = this->head->next;
 				return;
@@ -108,32 +108,39 @@ public:
 			H = H->next;
 		}
 	}
-//удаление всех элементов со значением val
+
 	void delall(T val) {
-		while((this->head!=nullptr)&&(this->head->value == val))
+
+		if (this->head == nullptr)
+		{
+			return;
+		}
+		if ((this->head->value == val) && (this->head == this->tail))
+		{
+			this->head = nullptr;
+			this->tail = nullptr;
+			return;
+		}
+		Node<T>*H = this->head;
+		if(this->head->value == val)
 		{
 			this->head = this->head->next;
 		}
-		Node<T>(*H) = this->head;
-		while (H != nullptr) {
-
-			if ((H->next != nullptr) && (H->next->next != nullptr))
+		while(H!=nullptr)
+		{
+			if((H->next!=nullptr)&&(H->next->value==val))
 			{
-				if (H->next->value == val) {
-
-					H->next = H->next->next;
-					continue;
-				}
-			}
-			if ((H->next != nullptr) && (H->next->next == nullptr))
-			{
-				if (H->next->value == val) {
+				if(H->next->next==nullptr)
+				{
 					this->tail = H;
-					this->tail->next = nullptr;
-					continue;
 				}
+
+				H->next = H->next->next;
 			}
+			else
+			{
 			H = H->next;
+			}
 		}
 		if ((this->head != nullptr) && (this->head->next == nullptr))
 		{
@@ -145,11 +152,19 @@ public:
 		}
 	}
 
+	void dellist() 
+	{
+		this->head = nullptr;
+		this->tail = nullptr;
+	}
+
+
+
 	void printstr() {
 		Node<T>* H = this->head;
-		while (H != nullptr) 
+		while (H != nullptr)
 		{
-			cout << H->value<<endl;
+			cout << H->value << endl;
 			H = H->next;
 		}
 	}
@@ -162,29 +177,29 @@ private:
 
 
 
-int main() 
+int main()
 {
-	Node<int> *pa = new Node<int>(1);
+	Node<int> *pa = new Node<int>(2);
 	Node<int> *pb = new Node<int>(1);
-	Node<int> *pc = new Node<int>(1);
+	Node<int> *pc = new Node<int>(2);
 	Node<int> *pd = new Node<int>(1);
 	Node<int> *p1 = new Node<int>(1);
-	Node<int> *p2 = new Node<int>(2);
-	Node<int> *p3 = new Node<int>(1);
+	Node<int> *p2 = new Node<int>(1);
+	Node<int> *p3 = new Node<int>(2);
 	Node<int> *p4 = new Node<int>(1);
 	Node<int> *p5 = new Node<int>(1);
 
-	
+
 	LinkedList<int> obg;
 	obg.add_in_tail(pa);
 	obg.add_in_tail(pb);
-	obg.add_in_tail(pc);
-	obg.add_in_tail(pd);
-	obg.add_in_tail(p1);
-	obg.add_in_tail(p2);
-	obg.add_in_tail(p3);
-	obg.add_in_tail(p4);
-	obg.add_in_tail(p5);
+	//obg.add_in_tail(pc);
+	//obg.add_in_tail(pd);
+	//obg.add_in_tail(p1);
+	//obg.add_in_tail(p2);
+	//obg.add_in_tail(p3);
+	//obg.add_in_tail(p4);
+	//obg.add_in_tail(p5);
 
 	Node<int>*P;
 	P = obg.find(0);
@@ -194,7 +209,9 @@ int main()
 	obg.delall(1);
 
 	obg.printstr();
-	
+
+	cout <<"\n"<< obg.getvalhead();
+
 
 	return 0;
 }

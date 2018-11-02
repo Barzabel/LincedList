abd955cd5f2c520444064f40350c5cdc5e7f96fe
Barzabel <kochenko1995@mail.ru>
2018-11-02 21:22:46 +0300

    add tests

--- main.cpp
+++ main.cpp
@@ -44,14 +44,14 @@ public:
 		this->count++;
 	}
 
-	T getvalhead()
+	Node<T>* gethead()
 	{
-		return this->head->value;
+		return this->head;
 	}
 
-	T getvaltail()
+	Node<T>* gettail()
 	{
-		return this->tail->value;
+		return this->tail;
 	}
 
 	Node<T>* find(T val) {
@@ -230,6 +230,7 @@ private:
 	unsigned int count;
 	Node<T> *head;
 	Node<T> *tail;
+	
 };
 
 
@@ -242,43 +243,141 @@ private:
 
 int main()
 {
-	Node<int> *pa = new Node<int>(2);
-	Node<int> *pb = new Node<int>(2);
-	Node<int> *pc = new Node<int>(2);
-	Node<int> *pd = new Node<int>(4);
-	Node<int> *p1 = new Node<int>(2);
+	// test 1 class Node
+	cout << "test 1 class Node" << endl;
+	Node<int> *p =new Node<int>(1);
+	if ((p->next == nullptr) && (p->value = 1)){
+		cout << "true \n"<<endl;}
+	else {
+		cout << "false \n" << endl;
+	}
+
+	delete p;
+	p = nullptr;
+	// test 2 class LinkedList
+	cout << "test 2 class LinkedList" << endl;
+	LinkedList<int>*obg= new LinkedList<int>;
+	if ((obg->gethead() == nullptr) && (obg->gettail() == nullptr)&&(obg->getsize()==0)) {
+		cout << "true \n" << endl;
+	}
+	else {
+		cout << "false \n" << endl;
+	}
+	obg->~LinkedList();
+	obg=nullptr;
+	// test 3 class LinkedList. void add_in_tail(Node<T> *item)
+
+
+	cout << "test 3 class LinkedList add_in_tail ferst element" << endl;
+	obg = new LinkedList<int>;
+	p = new Node<int>(1);
+	obg->add_in_tail(p);
+	if ((obg->gethead() == obg->gettail())  && (obg->getsize() == 1)) {
+		cout << "true \n" << endl;
+	}
+	else {
+		cout << "false \n" << endl;
+	}
+	// test 4 class LinkedList. void add_in_tail(Node<T> *item)
+
+	cout << "test 4 class LinkedList add_in_tail too element" << endl;
 	Node<int> *p2 = new Node<int>(2);
-	Node<int> *p3 = new Node<int>(2);
-	Node<int> *p4 = new Node<int>(2);
-	Node<int> *p5 = new Node<int>(2);
-	Node<int> *p6 = new Node<int>(2);
-
-	LinkedList<int> obg;
-	obg.add_in_tail(pa);
-	obg.add_in_tail(pb);
-	obg.add_in_tail(pc);
-	
-	
+	obg->add_in_tail(p2);
+	if ((obg->gethead() != obg->gettail()) && (obg->getsize() == 2)&&(obg->gettail()->value==2)&&(obg->gettail()->next==nullptr)) {
+		cout << "true \n" << endl;
+	}
+	else {
+		cout << "false \n" << endl;
+	}
 
 
-	LinkedList<int> obg2;
+	// test 5 class LinkedList. void add_in_position(Node<T>*position, Node<T>*value)
+	cout << "test 5 class LinkedList add_in_position" << endl;
+	Node<int> *p3 = new Node<int>(3);
+	obg->add_in_position(obg->gethead(), p3);
 
-	obg2.add_in_tail(p1);
-	obg2.add_in_tail(p2);
-	obg2.add_in_tail(p3);
-	obg2.add_in_tail(p4);
-
-	
-
-	obg.add_in_position(pc, pd);
-	
-
-
-
-	LinkedList<int>* obg3;
-	obg3 = obg2.findall(2);
-	obg3->printstr();
+	if((obg->gethead()->next==p3)&&(p3->next==obg->gettail())&&(obg->getsize()==3))
+	{
+		cout << "true \n" << endl;
+	}
+	else 
+	{
+		cout << "false \n" << endl;
+	}
 
 
+	// test 6 class LinkedList. void add_in_position(Node<T>*position, Node<T>*value) in taill
+	cout << "test 6 class LinkedList add_in_position in tail" << endl;
+	Node<int> *p4 = new Node<int>(4);
+	obg->add_in_position(obg->gettail(), p4);
+
+	if ((obg->gettail() == p4) && (p4->next == nullptr) && (obg->getsize() == 4))
+	{
+		cout << "true \n" << endl;
+	}
+	else
+	{
+		cout << "false \n" << endl;
+	}
+	// test 7 class LinkedList.find
+	cout << "test 6 class LinkedList  find()" << endl;
+	Node<int> *p5 = obg->find(4);
+	if ((obg->gettail() == p5) && (p5->next == nullptr) && (p5->value== 4))
+	{
+		cout << "true \n" << endl;
+	}
+	else
+	{
+		cout << "false \n" << endl;
+	}
+	//  test 7 class LinkedList.findall
+	Node<int> *p6 =new Node<int>(4);
+	obg->add_in_tail(p6);
+	LinkedList<int>*obg2 = obg->findall(4);
+	cout << "test 7 class LinkedList  findall()" << endl;
+	if ((obg2->gettail()->value == 4) && (obg2->gettail()->next == nullptr) && (obg2->getsize() == 2)&&(obg2->gethead()->value==4))
+	{
+		cout << "true \n" << endl;
+	}
+	else
+	{
+		cout << "false \n" << endl;
+	}
+	//  test 8 class LinkedList. del
+	cout << "test 7 class LinkedList  del()" << endl;
+	Node<int> *p7 = new Node<int>(4);
+	obg->add_in_tail(p7);
+	obg->del(4, true);
+
+	if(obg->find(4)==nullptr)
+	{
+		cout << "true \n" << endl;
+	}
+	else
+	{
+		cout << "false \n" << endl;
+	}
+	//  test 9 class LinkedList.  dellist()
+	cout << "test 7 class LinkedList  dellist()" << endl;
+	obg->dellist();
+	if((obg->gethead()==nullptr)&&(obg->gettail()==nullptr))
+	{
+		cout << "true \n" << endl;
+	}
+	else
+	{
+		cout << "false \n" << endl;
+	}
+
+
+
+
+
+	delete p7;
+	delete p6;
+	delete p5;
+	delete p3;
+	delete p2;
+	delete p;
 	return 0;
 }
